b792836e8d8a2239f1c6bfad3063aac9ce9d65e3
sthomas56 <sthomas56@studentmail.nait.ca>
2018-01-18 15:26:19 -0700

    setting up project

---
+++ stWebsite/Small Projects/SimpleCalculator.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="SimpleCalculator.aspx.cs" Inherits="Small_Projects_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" Runat="Server">
+
+    <h1>Simple Calculator</h1>
+
+    <asp:Label ID="errorLabel" runat="server" Text=""></asp:Label>
+
+     <br /> <br />
+    <asp:Label ID="Label1" runat="server" Text="First Value:"></asp:Label>
+    <asp:TextBox ID="firstNumber_TB" runat="server"></asp:TextBox>
+
+    <br />
+    <asp:Label ID="Label2" runat="server" Text="Second Value:"></asp:Label>
+    <asp:TextBox ID="secondNumber_TB" runat="server"></asp:TextBox>
+
+    <br />
+
+    <asp:Button ID="add_BTN" runat="server" Text="+" OnClick="add_BTN_Click" />
+    <asp:Button ID="subtract_BTN" runat="server" Text="-" OnClick="subtract_BTN_Click" />
+    <asp:Button ID="multiply_BTN" runat="server" Text="*" OnClick="multiply_BTN_Click" />
+    <asp:Button ID="division_BTN" runat="server" Text="/" OnClick="division_BTN_Click" />
+
+    <br />
+
+    <asp:Label ID="answer" runat="server" Text=""></asp:Label>
+</asp:Content>
+
---
+++ stWebsite/Small Projects/SimpleCalculator.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Small_Projects_Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+
+    protected void add_BTN_Click(object sender, EventArgs e)
+    {
+        if (firstNumber_TB.Text.Length == 0 || secondNumber_TB.Text.Length == 0)
+        {
+            errorLabel.Text = "Please enter a numeric value in each text box";
+        }
+        else
+        {
+            int valueOne = int.Parse(firstNumber_TB.Text);
+            int valueTwo = int.Parse(secondNumber_TB.Text);
+
+            answer.Text = "Answer:" + (valueOne + valueTwo);
+        }
+    }
+
+    protected void subtract_BTN_Click(object sender, EventArgs e)
+    {
+        if (firstNumber_TB.Text.Length == 0 || secondNumber_TB.Text.Length == 0)
+        {
+            errorLabel.Text = "Please enter a numeric value in each text box";
+        }
+        else
+        {
+            int valueOne = int.Parse(firstNumber_TB.Text);
+            int valueTwo = int.Parse(secondNumber_TB.Text);
+
+            answer.Text = "Answer:" + (valueOne - valueTwo);
+        }
+    }
+
+    protected void multiply_BTN_Click(object sender, EventArgs e)
+    {
+        if (firstNumber_TB.Text.Length == 0 || secondNumber_TB.Text.Length == 0)
+        {
+            errorLabel.Text = "Please enter a numeric value in each text box";
+        }
+        else
+        {
+            int valueOne = int.Parse(firstNumber_TB.Text);
+            int valueTwo = int.Parse(secondNumber_TB.Text);
+
+            answer.Text = "Answer:" + (valueOne * valueTwo);
+        }
+    }
+
+    protected void division_BTN_Click(object sender, EventArgs e)
+    {
+        if (firstNumber_TB.Text.Length == 0 || secondNumber_TB.Text.Length == 0)
+        {
+            errorLabel.Text = "Please enter a numeric value in each text box";
+        }
+        else
+        {
+            int valueOne = int.Parse(firstNumber_TB.Text);
+            int valueTwo = int.Parse(secondNumber_TB.Text);
+
+            answer.Text = "Answer:" + (valueOne / valueTwo);
+        }
+    }
+}
